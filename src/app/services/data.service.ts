bc172011af396b68dfee5e57f65f76780d532a9c
camilo17 <cami172013@gmail.com>
2017-07-23 21:06:10 -0500

    Created Question interface.

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Question } from '../models/Question';
+
+@Injectable()
+export class DataService {
+  questions: Question[];
+
+  constructor() {
+
+    this.questions = [
+      {
+        text: 'What is your name?',
+        answer: 'My name is Camilo',
+        hide: true
+      },
+      {
+        text: 'What is your favorite color?',
+        answer: 'My favorite color is purple',
+        hide: true
+      },
+      {
+        text: 'What is your favorite langauge?',
+        answer: 'My favorite language is javaScript',
+        hide: true
+      }
+    ];
+  }
+
+}
